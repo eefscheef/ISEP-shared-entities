383242f5d2a9ae91d7aca62abe20b0eeb8c8cf43
s2516268 <e.a.devree@student.utwente.nl>
2024-12-06 17:34:54 +0100

    create User entity and UserRole enum, update shared-entities dependency

---
+++ src/main/kotlin/entity/User.kt
@@ -0,0 +1,14 @@
+package entity
+
+import enumerable.UserRole
+import jakarta.persistence.*
+
+@Entity
+open class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    open var id: Long = 0,
+    open var name: String = "",
+    open var email: String = "",
+    open var role: UserRole? = null
+)
