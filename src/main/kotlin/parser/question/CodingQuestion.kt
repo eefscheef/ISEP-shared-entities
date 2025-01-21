b1e001231d7b8c34d98f1f0bc55316707b481d05
s2516268 <e.a.devree@student.utwente.nl>
2025-01-21 12:22:39 +0100

    Add CodingQuestion

---
+++ src/main/kotlin/parser/question/CodingQuestion.kt
@@ -0,0 +1,20 @@
+package parser.question
+
+import ut.isep.management.model.entity.AssignmentType
+
+data class CodingQuestion(
+    override val id: Long?,
+    override val tags: List<String>,
+    override val description: String,
+    override val filePath: String,
+    val code: CodingFile,
+    val testCode: CodingFile,
+    val secretTestCode: CodingFile,
+) : Question {
+    override val type: AssignmentType = AssignmentType.CODING
+}
+
+data class CodingFile(
+    val filename: String,
+    val code: String
+)
