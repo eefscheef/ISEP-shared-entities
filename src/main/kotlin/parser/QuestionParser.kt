4a1301618b6b7c4af439779c92198aace9e8736b
s2516268 <e.a.devree@student.utwente.nl>
2025-01-21 14:39:18 +0100

    Add QuestionParser.parseCodingDirectory method

--- src/main/kotlin/parser/QuestionParser.kt
+++ src/main/kotlin/parser/QuestionParser.kt
@@ -1,9 +1,9 @@
 package parser
 
-import parser.question.MultipleChoiceQuestion
-import parser.question.OpenQuestion
-import parser.question.Question
+import parser.question.*
 import ut.isep.management.model.entity.AssignmentType
+import java.io.File
+import java.io.FileInputStream
 import java.io.Reader
 
 
@@ -15,24 +15,61 @@ class QuestionParser(
     /**
      * @throws QuestionParsingException
      */
-    fun parse(inputReader: Reader, fileName: String): Question {
+    fun parse(input: Reader, filePath: String): Question {
         try {
-            // Delegate parsing of frontmatter and body
-            val (metadata, body) = frontmatterParser.parse(inputReader, fileName)
-            val type: AssignmentType = AssignmentType.fromString(metadata.type)
-            return when (type) {
+            val (metadata, body) = frontmatterParser.parse(input, filePath)
+            return when (metadata.type) {
                 AssignmentType.MULTIPLE_CHOICE -> parseMultipleChoiceQuestion(body, metadata)
                 AssignmentType.OPEN -> parseOpenQuestion(body, metadata)
-                AssignmentType.CODING -> TODO()
+                AssignmentType.CODING -> throw QuestionParsingException(
+                    message = "Trying to parse coding question in theoretical questions directory.",
+                    context = "Input: $filePath\n",
+                )
             }
         } catch (e: Exception) {
             throw QuestionParsingException(
                 message = "Failed to parse question.",
-                context = "Input: $fileName\nError: ${e.message}",
+                context = "Input: ${filePath}\nError: ${e.message}",
                 cause = e
             )
         }
     }
+
+    fun parseCodingDirectory(directory: File): CodingQuestion {
+        try {
+            val mdFiles: Array<File>? = directory.listFiles { file -> file.extension == "md" }
+            requireNotNull(mdFiles) {
+                "Failed to list files in directory: ${directory.absolutePath}"
+            }
+            require(mdFiles.size == 1) {
+                "Must provide exactly one description markdown file per coding question directory. Found ${mdFiles.size} files."
+            }
+            val mdFile: File = mdFiles[0]
+            FileInputStream(mdFile).bufferedReader().use { br ->
+                val (metadata, body) = frontmatterParser.parse(br, mdFile.name)
+                val codeFile = CodingFile(metadata.codeFilename!!, directory.resolve(metadata.codeFilename).readText())
+                val testFile = CodingFile(metadata.testFilename!!, directory.resolve(metadata.testFilename).readText())
+                val secretTestFile =
+                    CodingFile(metadata.secretTestFilename!!, directory.resolve(metadata.secretTestFilename).readText())
+                return CodingQuestion(
+                    id = metadata.id,
+                    tags = metadata.tags,
+                    filePath = metadata.originalFilePath,
+                    description = body,
+                    code = codeFile,
+                    testCode = testFile,
+                    secretTestCode = secretTestFile
+                )
+            }
+        } catch (e: Exception) {
+            throw QuestionParsingException(
+                message = "Failed to parse question.",
+                context = "Input: ${directory.path}\nError: ${e.message}",
+                cause = e
+            )
+        }
+    }
+
 
     // Parsing logic for question types remains unchanged
     private fun parseMultipleChoiceQuestion(body: String, metadata: Frontmatter): MultipleChoiceQuestion {
