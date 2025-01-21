a0838c40ca59f7ba9e9ecf5a6019f6eaee526f08
s2516268 <e.a.devree@student.utwente.nl>
2025-01-21 12:43:48 +0100

    Mark Frontmatter.id as @JsonIgnore so it doesn't get parsed from the frontmatter like we originally intended. These IDs should be parsed from filenames.

--- src/main/kotlin/parser/Frontmatter.kt
+++ src/main/kotlin/parser/Frontmatter.kt
@@ -1,6 +1,7 @@
 package parser
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonProperty
 
 
@@ -9,8 +10,10 @@ data class Frontmatter @JsonCreator constructor(
     @JsonProperty("tags") val tags: List<String>,
     @JsonProperty("availablePoints") val availablePoints: Int = 1
 ) {
+    @JsonIgnore
     lateinit var originalFilePath: String
     val baseFilePath: String
         get() = QuestionIDUtil.removeQuestionIDIfPresent(originalFilePath)
+    @JsonIgnore
     var id: Long? = null
 }
