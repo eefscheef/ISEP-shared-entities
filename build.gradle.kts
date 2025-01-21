0a0f0bb59dcb3cc56504ca1c00a266bd7546f774
s2516268 <e.a.devree@student.utwente.nl>
2025-01-21 12:23:17 +0100

    Move Jackson dependencies to shared-entities so the parser can use them

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.1")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
 }
