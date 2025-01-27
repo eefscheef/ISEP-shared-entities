c979de1579821e78f12aeb72c392b20561205eef
s2516268 <e.a.devree@student.utwente.nl>
2025-01-27 18:23:07 +0100

    Split up Frontmatter into CodingFrontMatter, OpenFrontmatter, and Frontmatter (for multiple-choice)

--- src/main/kotlin/parser/Frontmatter.kt
+++ src/main/kotlin/parser/Frontmatter.kt
@@ -1,41 +1,64 @@
 package parser
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.*
 import ut.isep.management.model.entity.AssignmentType
 
-
-data class Frontmatter @JsonCreator constructor(
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type",
+    visible = true
+)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = OpenFrontmatter::class, name = "open"),
+    JsonSubTypes.Type(value = Frontmatter::class, name = "multiple-choice"),
+    JsonSubTypes.Type(value = CodingFrontmatter::class, name = "coding")
+)
+open class Frontmatter @JsonCreator constructor(
     @JsonProperty("type", required = true) val type: AssignmentType,
     @JsonProperty("tags", required = true) val tags: List<String>,
     @JsonProperty("points", required = true) val availablePoints: Int,
     @JsonProperty("seconds", required = true) val availableSeconds: Long,
-    @JsonProperty("language") val language: String?,
-    @JsonProperty("code") val codeFilename: String?,
-    @JsonProperty("test") val testFilename: String?,
-    @JsonProperty("secret-test") val secretTestFilename: String?
 ) {
     @JsonIgnore
     lateinit var originalFilePath: String
     val baseFilePath: String
         get() = QuestionIDUtil.removeQuestionIDIfPresent(originalFilePath)
+
     @JsonIgnore
     var id: Long? = null
 
-
     init {
-        require(availablePoints > 0) {"Assignments must be worth at least 1 point"}
-        require(availableSeconds > 0) {"Recommended assignment time must be at least 1 second"}
+        require(availablePoints > 0) { "Assignments must be worth at least 1 point" }
+        require(availableSeconds > 0) { "Recommended assignment time must be at least 1 second" }
+    }
+}
 
-        if (type == AssignmentType.CODING) {
-            require(codeFilename != null && testFilename != null && secretTestFilename != null && language != null) {
-                "For a coding assignment .md file, it is necessary to provide the language, code, test, and secret-test properties"
-            }
-        } else {
-            require(codeFilename == null && testFilename == null && secretTestFilename == null && language == null) {
-                "for a non-coding assignment, there should be no language, code, test, or secret-test properties provided"
-            }
-        }
+class OpenFrontmatter @JsonCreator constructor(
+    @JsonProperty("type", required = true) type: AssignmentType,
+    @JsonProperty("tags", required = true) tags: List<String>,
+    @JsonProperty("points", required = true) availablePoints: Int,
+    @JsonProperty("seconds", required = true) availableSeconds: Long,
+    @JsonProperty("referenceAnswer", required = false) val referenceAnswer: String? = null
+) : Frontmatter(type, tags, availablePoints, availableSeconds) {
+    init {
+        require(type == AssignmentType.OPEN)
+    }
+}
+
+class CodingFrontmatter @JsonCreator constructor(
+    @JsonProperty("type", required = true) type: AssignmentType,
+    @JsonProperty("tags", required = true) tags: List<String>,
+    @JsonProperty("points", required = true) availablePoints: Int,
+    @JsonProperty("seconds", required = true) availableSeconds: Long,
+    @JsonProperty("language", required = true) val language: String,
+    @JsonProperty("code", required = true) val codeFilename: String,
+    @JsonProperty("test", required = true) val testFilename: String,
+    @JsonProperty("secret-test", required = true) val secretTestFilename: String,
+    @JsonProperty("reference-code", required = false) val referenceCodeFilename: String? = null,
+    @JsonProperty("reference-test", required = false) val referenceTestFilename: String? = null,
+) : Frontmatter(type, tags, availablePoints, availableSeconds) {
+    init {
+        require(type == AssignmentType.CODING)
     }
 }
