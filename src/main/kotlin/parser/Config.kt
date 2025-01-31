7975143328704903a720d6cabc56de88c55d6f6b
s2516268 <e.a.devree@student.utwente.nl>
2025-01-31 21:48:56 +0100

    Update Config

--- src/main/kotlin/parser/Config.kt
+++ src/main/kotlin/parser/Config.kt
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Config @JsonCreator constructor(
-    @JsonProperty("tag-options")
-    val tagOptions: List<String>,
-    @JsonProperty("question-options")
-    val questionOptions: List<String>,
+    @JsonProperty("assessments")
+    val tagOptions: List<String>
 )
