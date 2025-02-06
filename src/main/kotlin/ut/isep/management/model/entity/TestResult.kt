e6e96e1ad0b765de94482487e91846cd9535bb6d
s2516268 <e.a.devree@student.utwente.nl>
2025-02-06 20:27:16 +0100

    remove TestResult.createTestResult

--- src/main/kotlin/ut/isep/management/model/entity/TestResult.kt
+++ src/main/kotlin/ut/isep/management/model/entity/TestResult.kt
@@ -19,16 +19,4 @@ open class TestResult(
         JoinColumn(name = "invite_id")
     )
     open var solvedAssignmentCoding: SolvedAssignmentCoding? = null,
-) : BaseEntity<Long> {
-
-    companion object {
-        fun createTestResult(name: String, message: String? = null, passed: Boolean, solvedAssignmentCoding: SolvedAssignmentCoding): TestResult {
-            val testResult: TestResult = if (message != null) {
-                TestResult(name = name, message = message, passed = passed, solvedAssignmentCoding = solvedAssignmentCoding)
-            } else {
-                TestResult(name = name, passed = passed, solvedAssignmentCoding = solvedAssignmentCoding)
-            }
-            return testResult
-        }
-    }
-}
+) : BaseEntity<Long>
