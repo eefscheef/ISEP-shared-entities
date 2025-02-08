1aa30f74d10d9863412cf663d62542c8af897624
JSweers <j.e.sweers@student.utwente.nl>
2025-02-08 16:59:46 +0100

    Update build.gradle.kts to use testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,13 +13,23 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.1")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.1")
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.4") {
+        exclude("org.junit.vintage:junit-vintage-engine")
+        exclude("org.mockito:mockito-core")
+    }
+    testImplementation(kotlin("test"))
 }
 
 
 tasks.test {
     useJUnitPlatform()
+    jvmArgs = listOf("-XX:+EnableDynamicAgentLoading")
 }
 kotlin {
     jvmToolchain(21)
